9f4d650b24873c47e6346b6743f92d5889dbe045
YuI <474766481@qq.com>
2020-11-25 00:07:27 +0800

    add:第六周作业

---
+++ Week_06/HomeWork.md
@@ -0,0 +1,9 @@
+# 作业
+
+## Week06 作业题目（周六）
+
+###2.（必做）基于电商交易场景（用户、商品、订单），设计一套简单的表结构，提交 DDL 的 SQL 文件到 Github（后面 2 周的作业依然要是用到这个表结构）。
+
+- [买家信息表](./src/main/resources/db_ecommerce.t_purchaser.sql)
+- [商品信息表](./src/main/resources/db_ecommerce.t_commodity.sql)
+- [订单信息表](./src/main/resources/db_ecommerce.t_order.sql)
---
+++ Week_06/src/main/resources/db_ecommerce.t_order.sql
@@ -0,0 +1,13 @@
+CREATE TABLE t_order
+(
+    id                VARCHAR(64)    NOT NULL COMMENT 'ID 使用雪花算法生成主键',
+    purchaser_id      VARCHAR(64)    NOT NULL COMMENT '买家ID 买家ID',
+    commodity_id      VARCHAR(64)    NOT NULL COMMENT '商品ID 商品ID',
+    merchant_id       VARCHAR(64)    NOT NULL COMMENT '商家ID 商家ID',
+    transaction_price DECIMAL(32, 8) NOT NULL COMMENT '交易价格 交易时的价格（分）',
+    transaction_count BIGINT         NOT NULL COMMENT '交易数量 交易数量',
+    order_status      VARCHAR(32)    NOT NULL DEFAULT 1 COMMENT '订单状态 订单状态1-交易创建/2-买家付款/3-卖家发货/4-货品到达/5-订单成功/6-订单评价完成/7-订单失败',
+    create_datetime   DATETIME       NOT NULL COMMENT '创建时间 创建时间',
+    update_datetime   DATETIME       NOT NULL COMMENT '更新时间 更新时间',
+    PRIMARY KEY (id)
+) COMMENT = '订单表，包括但不限于订单买家ID、商品ID、购买时的价格、购买的数量';
