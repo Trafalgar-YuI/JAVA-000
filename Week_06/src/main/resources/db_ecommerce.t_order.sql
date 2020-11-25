d3834282dfe19d0354935ff6fd4154ffac0d905d
刘艺 <liuyi-4@thunisoft.com>
2020-11-25 09:50:03 +0800

    add:week_06 周六的作业

--- Week_06/src/main/resources/db_ecommerce.t_order.sql
+++ Week_06/src/main/resources/db_ecommerce.t_order.sql
@@ -4,6 +4,7 @@ CREATE TABLE t_order
     purchaser_id      VARCHAR(64)    NOT NULL COMMENT '买家ID 买家ID',
     commodity_id      VARCHAR(64)    NOT NULL COMMENT '商品ID 商品ID',
     merchant_id       VARCHAR(64)    NOT NULL COMMENT '商家ID 商家ID',
+    address           VARCHAR(1024)  NOT NULL COMMENT '发货地址',
     transaction_price DECIMAL(32, 8) NOT NULL COMMENT '交易价格 交易时的价格（分）',
     transaction_count BIGINT         NOT NULL COMMENT '交易数量 交易数量',
     order_status      VARCHAR(32)    NOT NULL DEFAULT 1 COMMENT '订单状态 订单状态1-交易创建/2-买家付款/3-卖家发货/4-货品到达/5-订单成功/6-订单评价完成/7-订单失败',
