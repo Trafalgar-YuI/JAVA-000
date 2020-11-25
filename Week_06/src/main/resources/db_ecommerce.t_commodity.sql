3158ea14b3a34c140350f04d4cf1535aff046eb7
YuI <474766481@qq.com>
2020-11-25 22:14:17 +0800

    fix:修改 sql

--- Week_06/src/main/resources/db_ecommerce.t_commodity.sql
+++ Week_06/src/main/resources/db_ecommerce.t_commodity.sql
@@ -1,17 +1,16 @@
 CREATE TABLE t_commodity
 (
     id                      VARCHAR(64)     NOT NULL COMMENT '商品ID 使用雪花算法生成主键',
-    merchant_id             VARCHAR(64)     NOT NULL COMMENT '商家ID 商家ID',
-    name                    VARCHAR(128)    NOT NULL COMMENT '商品名称 商品名称',
-    type                    VARCHAR(32)     NOT NULL COMMENT '商品类型 商品的分类类型',
-    count                   BIGINT          NOT NULL DEFAULT 0 COMMENT '商品数量 商品数量',
-    price                   DECIMAL(32, 8)  NOT NULL DEFAULT 100000000 COMMENT '商品价格 商品价格（分）',
-    buy_count               BIGINT          NOT NULL DEFAULT 0 COMMENT '商品购买数量 商品购买数量',
-    comment_level           DECIMAL(32, 10) NOT NULL DEFAULT 5.0 COMMENT '商品的评价等级 商品的评价等级（定时任务计算）',
-    discount_ratio          DECIMAL(32, 10)   NOT NULL DEFAULT 100 COMMENT '折扣比例 折扣比例',
-    discount_start_datetime DATETIME        NOT NULL COMMENT '打折开始时间 打折开始时间',
-    discount_end_datetime   DATETIME        NOT NULL COMMENT '打折结束时间 打折结束时间',
-    create_datetime         DATETIME        NOT NULL DEFAULT now() COMMENT '创建时间 创建时间',
-    update_datetime         DATETIME        NOT NULL DEFAULT now() COMMENT '更新时间 更新时间',
+    name                    VARCHAR(128)    NOT NULL COMMENT '商品名称',
+    type                    VARCHAR(32)     NOT NULL COMMENT '商品的分类类型',
+    count                   BIGINT          NOT NULL DEFAULT 0 COMMENT '商品数量',
+    price                   DECIMAL(32, 8)  NOT NULL DEFAULT 100000000 COMMENT '商品价格（分）',
+    buy_count               BIGINT          NOT NULL DEFAULT 0 COMMENT '商品购买数量',
+    comment_level           DECIMAL(32, 10) NOT NULL DEFAULT 5.0 COMMENT '商品的评价等级（定时任务计算）',
+    discount_ratio          DECIMAL(32, 10)   NOT NULL DEFAULT 100 COMMENT '折扣比例',
+    discount_start_datetime DATETIME        NOT NULL COMMENT '打折开始时间',
+    discount_end_datetime   DATETIME        NOT NULL COMMENT '打折结束时间',
+    create_datetime         DATETIME        NOT NULL DEFAULT now() COMMENT '创建时间',
+    update_datetime         DATETIME        NOT NULL DEFAULT now() COMMENT '更新时间',
     PRIMARY KEY (id)
-) COMMENT = '商品信息表，包括但不限于商品名称、价格、数量、商户ID等';
+) COMMENT = '商品信息表，包括但不限于商品名称、价格、数量等';
--- Week_06/src/main/resources/db_ecommerce.t_purchaser.sql
+++ Week_06/src/main/resources/db_ecommerce.t_purchaser.sql
@@ -1,14 +1,14 @@
 CREATE TABLE t_purchaser
 (
     id              VARCHAR(64)    NOT NULL COMMENT '买家ID 使用雪花算法生成主键',
-    login_id        VARCHAR(64)    NOT NULL COMMENT '买家登录帐号 买家登录帐号',
-    login_password  VARCHAR(64)    NOT NULL COMMENT '买家登录密码 买家登录密码-存储MD5加密的值',
-    nick_name       VARCHAR(32)    NOT NULL COMMENT '昵称 昵称',
-    id_card         VARCHAR(32)    NOT NULL COMMENT '身份证号 身份证号',
-    phone_number    VARCHAR(32)    NOT NULL COMMENT '手机号 手机号',
-    balance         DECIMAL(32, 8) NOT NULL DEFAULT 0 COMMENT '余额 余额（分）',
-    account_status  VARCHAR(32)    NOT NULL DEFAULT 1 COMMENT '帐号状态 帐号状态1-正常/2-注销/3-冻结',
-    create_datetime DATETIME       NOT NULL DEFAULT now() COMMENT '创建时间 创建时间',
-    update_datetime DATETIME       NOT NULL DEFAULT now() COMMENT '更新时间 更新时间',
+    login_id        VARCHAR(64)    NOT NULL COMMENT '买家登录帐号',
+    login_password  VARCHAR(64)    NOT NULL COMMENT '买家登录密码 存储MD5加密的值',
+    nick_name       VARCHAR(32)    NOT NULL COMMENT '昵称',
+    id_card         VARCHAR(32)    NOT NULL COMMENT '身份证号',
+    phone_number    VARCHAR(32)    NOT NULL COMMENT '手机号',
+    balance         DECIMAL(32, 8) NOT NULL DEFAULT 0 COMMENT '余额（分）',
+    account_status  VARCHAR(32)    NOT NULL DEFAULT 1 COMMENT '帐号状态 1-正常/2-注销/3-冻结',
+    create_datetime DATETIME       NOT NULL DEFAULT now() COMMENT '创建时间',
+    update_datetime DATETIME       NOT NULL DEFAULT now() COMMENT '更新时间',
     PRIMARY KEY (id)
 ) COMMENT = '存储买家的账户，包括但不限于帐号、密码、昵称、余额';
